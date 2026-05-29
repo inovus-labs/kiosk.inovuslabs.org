953990310a07e79456a27085957910f20dfb075d
Arjun Krishna <arjun12345krishna@gmail.com>
2026-05-29 14:48:26 +0530

    fix: add outputFileTracingExcludes to next.config for improved build performance

--- worker/next.config.ts
+++ worker/next.config.ts
@@ -8,6 +8,13 @@ const nextConfig = {
     localPatterns: [{ pathname: '/api/media/file/**' }],
   },
   serverExternalPackages: ['jose'],
+  outputFileTracingExcludes: {
+    '*': [
+      '**/next/dist/compiled/@vercel/og/**',
+      '**/next/dist/compiled/sharp/**',
+      '**/next/dist/compiled/babel/**',
+    ],
+  },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
